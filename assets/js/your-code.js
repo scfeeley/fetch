ddb2bf7250ee8353c26e69194e92cccdb5f92d71
Shannon Feeley <scfeeley@wpi.edu>
2023-09-13 12:11:20 -0600

    initial commit

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,47 @@
 export function getAllDogs() {
-    // Your code here
+    return fetch('/dogs');
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+    return fetch('/dogs/2')
 }
 
 export function postNewDog() {
-    // Your code here
+    let url ='/dogs';
+    let name = 'dog';
+    let age = 4;
+    let body = new URLSearchParams({
+        name: name,
+        age: age
+    });
+    let options = {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded"},
+        body: body
+    };
+
+    return fetch(url, options);
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+    let url = "/dogs";
+    let body = new URLSearchParams({
+        name: name,
+        age : age,
+    });
+    let options = {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: body
+    };
+     return fetch(url, options);
 }
 
 export function deleteDog(id) {
-      // Your code here
+      let url = `/dogs/${id}/delete`
+      let options = {
+        method : "POST",
+        headers: { "AUTH": "ckyut5wau0000jyv5bsrud90y"}
+      }
+      return fetch(url, options)
 }
